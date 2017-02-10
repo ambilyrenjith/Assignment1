a5ec05c9082916b29efb720b72bc5e783e2e3859
ambilyrenjith <ambilyrenjith@yahoo.com>
2017-02-09 21:22:35 -0800

    Newly added files.

---
+++ script.js
@@ -0,0 +1,235 @@
+//STEP 1
+/*
+var some_Month;
+function theMonth
+currentMonth
+var summer_month
+myLibraryaFunction */
+
+
+//STEP 2
+/*
+4.1 -numeric literal
+"Hello"- String literal
+true -Boolean expression
+null -null literal expression
+*/
+
+//STEP 3
+/*
+Complex Expressions
+var x = Math.PI *radius * radius;
+var y = 6(10/ 2) + 5;
+*/
+
+//STEP 4
+/*
+
+var strFirstname;
+var strLastname;
+var strAddress;
+var strCity;
+var strState;
+var intZipcode;
+var intAge;
+var strReferralsource;
+var strMayweContactyou;
+*/
+
+//STEP 5
+/*
+var strFirstname;
+strFirstname ="Ambili"
+    Or
+var strFirstname = "Ambili";
+
+var strLastname;
+strLastname = "Krishnankutty"; 
+    Or
+var strLastname = "Krishnankutty";
+
+var strAddress;
+strAddress="Geneseecove";
+    Or
+var strAddress  = "Geneseecove";
+
+var strCity;
+strCity="SanDiego"
+    Or
+var strCity = "SanDiego";
+
+var strState;
+strState = "California";
+    Or
+var strState = "California";
+
+var intZipcode;
+intZipcode = 92122;
+    Or
+var intZipcode = 92122;
+
+var intAge;
+intAge = 30;
+    Or
+var intAge = 30;
+
+var strReferralsource;
+strReferralsource="dsff";
+    Or
+var strReferralsource = "dsff";
+
+var strMayweContactyou;
+strMayweContactyou="abcd@yahoo.com";
+    Or
+var strMayweContactyou = "abcd@yahoo.com";
+
+    Or    
+
+ var strFirstname="Ambili", strLastname = "Krishnankutty", strAddress  = "Geneseecove", strCity = "SanDiego", strState = "California", intZipcode = 92122, intAge = 30, strReferralsource = "dsff", strMayweContactyou="abcd@yahoo.com";  
+*/
+
+
+//STEP 6
+/*
+var x = 30 + "Hello";
+window.console.log(x);
+
+var y = true + "Hello";
+window.console.log(y);
+
+var z = 60 + true;
+window.console.log(z);
+*/
+
+//STEP 7 
+
+
+/*window.console.log("I like" + " " + x);
+var x = "pets"; */
+
+//STEP 8
+
+
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."';
+console.log(someString);
+
+
+//STEP 9
+/*
+var x = null;
+window.console.log(x);
+
+window.console.log(y);
+var y; */
+
+//STEP 10
+/*
+window.console.log(typeof("Hello"));
+window.console.log(typeof(70));
+window.console.log(typeof(false));
+window.console.log(typeof(null));
+window.console.log(typeof(call)); */
+
+//STEP 11
+/*
+window.alert("Hello"+" "+"Ambili"+" "+ "welcome to the Javascript class"); */
+
+//STEP 12
+/*
+var name = "Ambili";
+window.alert("Hello"+" "+name+" "+ "welcome to the JavaScript class"); */
+
+
+//STEP 13
+/*
+var course = "JavaScript";
+var name = "Ambili";
+window.alert("Hello"+" "+name+" "+ "welcome to the"+ " " +course  +"class"); */
+
+//STEP 14
+/*
+var course = "JavaScript";
+var name = "Ambili";
+window.alert("Hello"+" "+name+"\n "+"welcome to the"+ " " +course  +"class"); */
+
+//STEP 15
+/*
+var name = window.prompt("Enter your name:"); */
+
+//STEP 16
+//var course = window.prompt("Enter your course");
+
+//STEP 17
+/*
+ var x = 10;
+ var y = 20;
+window.console.log("The sum of 10 and 20  is" + " " +(x+y));*/
+
+//STEP 18
+/*
+var x = 20;
+x+= 20;
+window.console.log(x); */
+
+//STEP 19
+/*
+var x = 20;
+x*=5;
+window.console.log(x);*/
+
+//STEP 20
+/*
+ var x = 20%3;
+x/= 1;
+window.console.log(x);
+
+*/
+
+//STEP 21
+/*
+var x = 20 ;
+var y = 10 ;
+if(x!=y && x > y)
+window.console.log(true);  
+*/
+
+//STEP 22
+
+/*
+var x = 5 ;
+var y = 10 ;
+if(x!=y && x > y)
+window.console.log(true); 
+else
+window.console.log(false);
+*/
+
+//STEP 23
+/*
+var widget = new Object();
+window.console.log(typeof(widget)); */
+
+//STEP 24
+/*
+var widget = new Object();
+window.console.log(widget instanceof Object);  
+*/
+
+//STEP 25
+/*
+var widget = new Object();
+window.console.log(widget instanceof Array); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
