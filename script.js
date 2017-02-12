fe4178938172ad9a56b4273146df5c4ad8dfc872
ambilyrenjith <ambilyrenjith@yahoo.com>
2017-02-12 14:33:20 -0800

    Initil draft

--- script.js
+++ script.js
@@ -1,47 +1,45 @@
 //STEP 1
 /*
-var some_Month;
+var someMonth;
 function theMonth
 currentMonth
-var summer_month
-myLibraryaFunction */
-
-
+var summerMonth
+myLibraryAFunction */
 //STEP 2
 /*
-4.1 -numeric literal
-"Hello"- String literal
-true -Boolean expression
-null -null literal expression
+4.1 - numeric literal
+"Hello" - String literal
+true - Boolean expression
+null - null literal expression
 */
-
 //STEP 3
 /*
 Complex Expressions
 var x = Math.PI *radius * radius;
 var y = 6(10/ 2) + 5;
 */
-
 //STEP 4
 /*
 
-var strFirstname;
-var strLastname;
+var strFirstName;
+var strLastName;
 var strAddress;
 var strCity;
 var strState;
-var intZipcode;
+var intZipCode;
 var intAge;
-var strReferralsource;
-var strMayweContactyou;
+var strReferralSource;
+var strMayWeContactYou;
 */
-
 //STEP 5
-/*
-var strFirstname;
-strFirstname ="Ambili"
+/*  
+
+ var strFirstName="Ambili", strLastname = "Krishnankutty", strAddress  = "Geneseecove", strCity = "SanDiego", strState = "California", intZipcode = 92122, intAge = 30, strReferralsource = "My friend", strMayweContactyou="abcd@yahoo.com";  
     Or
-var strFirstname = "Ambili";
+var strFirstName;
+strFirstName ="Ambili"
+    Or
+var strFirstName = "Ambili";
 
 var strLastname;
 strLastname = "Krishnankutty"; 
@@ -74,21 +72,16 @@ intAge = 30;
 var intAge = 30;
 
 var strReferralsource;
-strReferralsource="dsff";
+strReferralsource="My friend";
     Or
-var strReferralsource = "dsff";
+var strReferralsource = "My friend";
 
 var strMayweContactyou;
 strMayweContactyou="abcd@yahoo.com";
     Or
 var strMayweContactyou = "abcd@yahoo.com";
 
-    Or    
-
- var strFirstname="Ambili", strLastname = "Krishnankutty", strAddress  = "Geneseecove", strCity = "SanDiego", strState = "California", intZipcode = 92122, intAge = 30, strReferralsource = "dsff", strMayweContactyou="abcd@yahoo.com";  
-*/
-
-
+ */
 //STEP 6
 /*
 var x = 30 + "Hello";
@@ -100,28 +93,24 @@ window.console.log(y);
 var z = 60 + true;
 window.console.log(z);
 */
-
 //STEP 7 
-
-
-/*window.console.log("I like" + " " + x);
-var x = "pets"; */
-
+/*
+window.console.log("I like " + x);
+var x = "pets";
+*/
 //STEP 8
-
-
-var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."';
+/*
+//No its not a valid string. Corrected as below
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
 console.log(someString);
-
-
+*/
 //STEP 9
 /*
 var x = null;
 window.console.log(x);
-
+var y;
 window.console.log(y);
-var y; */
-
+*/
 //STEP 10
 /*
 window.console.log(typeof("Hello"));
@@ -129,62 +118,51 @@ window.console.log(typeof(70));
 window.console.log(typeof(false));
 window.console.log(typeof(null));
 window.console.log(typeof(call)); */
-
 //STEP 11
 /*
-window.alert("Hello"+" "+"Ambili"+" "+ "welcome to the Javascript class"); */
-
+window.alert("Hello " + "Ambili" + ", welcome to the Javascript class"); 
 //STEP 12
-/*
 var name = "Ambili";
-window.alert("Hello"+" "+name+" "+ "welcome to the JavaScript class"); */
-
-
+window.alert("Hello " + name + ", welcome to the JavaScript class");
 //STEP 13
-/*
 var course = "JavaScript";
 var name = "Ambili";
-window.alert("Hello"+" "+name+" "+ "welcome to the"+ " " +course  +"class"); */
-
+window.alert("Hello " + name + ", welcome to the " + course + " class");
 //STEP 14
-/*
 var course = "JavaScript";
 var name = "Ambili";
-window.alert("Hello"+" "+name+"\n "+"welcome to the"+ " " +course  +"class"); */
-
+window.alert("Hello " + name + "\n" + "Welcome to the " + course + " class");
+*/
 //STEP 15
 /*
-var name = window.prompt("Enter your name:"); */
-
+var name = window.prompt("Enter your name:");
+window.alert("Hello " + name + ", welcome to the JavaScript class");
 //STEP 16
-//var course = window.prompt("Enter your course");
-
-//STEP 17
+var course = window.prompt("Enter your course");
+window.alert("Hello " + name + ", welcome to the " + course + " class");
+*/
+//STEP 1
 /*
- var x = 10;
- var y = 20;
-window.console.log("The sum of 10 and 20  is" + " " +(x+y));*/
-
+x = 10;
+y = 20;
+console.log("The sum of 10 and 20 is " + (x + y));
+*/
 //STEP 18
 /*
 var x = 20;
 x+= 20;
 window.console.log(x); */
-
 //STEP 19
 /*
 var x = 20;
-x*=5;
+x *= 5;
 window.console.log(x);*/
-
 //STEP 20
 /*
- var x = 20%3;
-x/= 1;
+var x = 20 % 3;
+x /= 1;
 window.console.log(x);
-
 */
-
 //STEP 21
 /*
 var x = 20 ;
@@ -192,9 +170,7 @@ var y = 10 ;
 if(x!=y && x > y)
 window.console.log(true);  
 */
-
 //STEP 22
-
 /*
 var x = 5 ;
 var y = 10 ;
@@ -203,33 +179,16 @@ window.console.log(true);
 else
 window.console.log(false);
 */
-
 //STEP 23
 /*
 var widget = new Object();
 window.console.log(typeof(widget)); */
-
 //STEP 24
 /*
 var widget = new Object();
-window.console.log(widget instanceof Object);  
+window.console.log(widget instanceof Object);
 */
-
 //STEP 25
 /*
 var widget = new Object();
 window.console.log(widget instanceof Array); */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
